a9c5da05460150c50c226eb06621c1339883a3c9
LinhPham03 <phamthithuylinh190703@gmail.com>
2022-08-22 22:06:51 +0700

    even odd

---
+++ toantu3ngoi.py
@@ -0,0 +1,7 @@
+# Kiểm tra số nhập vào là số chẳn hoặc lẻ, sử dụng toán tử điều kiện ba ngôi
+num=int(input('Nhập 1 số bất kì: '))
+
+if (num%2 == 0):
+    print(num, "là số chẵn")
+else:
+    print(num, "là số lẻ")
